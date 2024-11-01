757201114f22dd9a747bd97ced6a0b5e696ef58e
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2024-11-01 18:03:43 -0400

    added symlink if check for symlink

--- postmake.lua
+++ postmake.lua
@@ -14,6 +14,58 @@ end
 postmake.appinstalldir = "~/.postmake"
 postmake.applicensefile = "LICENSE.txt"
 postmake.output = "./output/install"
+
+
+if postmake.target() == "symlink" then
+	-- This is great for debuging and quick fixing
+	local function executeorexit(str)
+		local exit = os.execute(str)
+
+		if exit == nil then
+			os.exit(1)
+		end
+
+		if exit ~= 0 then
+			print("running command failed : '" .. str .. "'")
+			os.exit(1)
+		end
+	end
+	local function symlink(input, output)
+		print("symlink " .. output .. " >> " .. input)
+
+		local issymlink = false
+		if issymlink then
+
+		else
+			if postmake.os.exist(output) then
+				if postmake.os.IsFile(output) then
+					postmake.os.rm(output)
+				else
+					postmake.os.rmall(output)
+				end
+			end
+
+			postmake.os.ln(input, output)
+		end
+	end
+
+
+	local thisinstalldir = postmake.path.absolute(postmake.appinstalldir) .. "/"
+
+	local mainfile = thisinstalldir .. postmake.installdir() .. "/" .. postmake.appname
+	if postmake.os.uname.os() == 'windows' then
+		mainfile = mainfile .. ".exe"
+	end
+	postmake.os.mkdirall(thisinstalldir .. "bin")
+	postmake.os.mkdirall(thisinstalldir .. "lua")
+
+	executeorexit("go build")
+	symlink(postmake.path.absolute("./postmake"), mainfile)
+	symlink(postmake.path.absolute("lua/api"), thisinstalldir .. "lua/definitions")
+
+	return
+end
+
 -- Short Hands
 local all = postmake.allconfig
 
